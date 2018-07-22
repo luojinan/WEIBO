eb658b67c95404fb24ce3b485c556125b69bdd8e
kingan <1017386624@qq.com>
2018-07-22 10:37:30 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <span id="wb_connect_btn" style="clear: both;float:right;"></span>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Fuck the shit'
+    }
+  },
+  methods:{
+    test(W){
+      W.widget.connectButton({
+          id: "wb_connect_btn", 
+          type:"3,2",
+          callback : {
+            login:function(item){  //登录后的回调函数
+              console.log(item)
+            },  
+            logout:function(){  //退出后的回调函数
+            }
+          }
+      })
+    }
+  },
+  mounted(){
+    WB2.anyWhere(this.test)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">  
+    首页
+    <span class="iconfont">&#xe653;</span>
+    <img src="http://i2.hdslb.com/bfs/archive/e486384587f45f4f0fd42f0b487aa72cf6ef0fd0.jpg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+.home {
+
+}
+</style>
